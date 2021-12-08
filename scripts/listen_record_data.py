ab123f2cc0f82c13d19ea90d4843a671e8bbc860
Haresh Karnan <haresh.miriyala@gmail.com>
2021-12-08 11:21:06 -0600

    using python2 to listen to the tf frames for localization ifno

--- scripts/listen_record_data.py
+++ scripts/listen_record_data.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import numpy as np
 import time
@@ -17,17 +17,18 @@ from scipy.spatial.transform import Rotation as R
 from tqdm import tqdm
 from parse_utils import BEVLidar
 import yaml
+import tf2_ros
 
 class ListenRecordData:
     def __init__(self, rosbag_play_process, config_path):
         self.rosbag_play_process = rosbag_play_process
         self.data = []
         lidar = message_filters.Subscriber('/velodyne_points', PointCloud2)
-        odom = message_filters.Subscriber('/aft_mapped_to_init', Odometry)
+        # odom = message_filters.Subscriber('/aft_mapped_to_init', Odometry)
         # joystick = message_filters.Subscriber('/bluetooth_teleop/joy', Joy)
         joystick = message_filters.Subscriber('/joystick', Joy)
 
-        ts = message_filters.ApproximateTimeSynchronizer([lidar, odom, joystick], 100, 0.05, allow_headerless=True)
+        ts = message_filters.ApproximateTimeSynchronizer([lidar, joystick], 100, 0.05, allow_headerless=True)
         ts.registerCallback(self.callback)
 
         # read the config file
@@ -41,7 +42,16 @@ class ListenRecordData:
                                          y_range=(-self.config['LIDAR_RANGE_METERS'], self.config['LIDAR_RANGE_METERS']),
                                          z_range=(0, self.config['LIDAR_HEIGHT_METERS']), resolution=self.config['RESOLUTION'])
 
-    def callback(self, lidar, odom, joystick):
+        self.tfBuffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tfBuffer)
+
+    @staticmethod
+    def convert_float64img_to_uint8(image):
+        image = np.clip(image, 0, 1)
+        image = (image * 255).astype(np.uint8)
+        return image
+
+    def callback(self, lidar, joystick):
         """[callback function for the approximate time synchronizer]
 
         Args:
@@ -52,33 +62,38 @@ class ListenRecordData:
         # get the bev lidar image
         lidar_points = pc2.read_points(lidar, skip_nans=True, field_names=("x", "y", "z"))
         bev_lidar_image = self.bevlidar_handler.get_bev_lidar_img(lidar_points)
+        bev_lidar_image = self.convert_float64img_to_uint8(bev_lidar_image)
 
         # show the image
         # cv2.imshow('bev_lidar', bev_lidar_image)
         # cv2.waitKey(1)
 
-        # get the localization data
-        x = odom.pose.pose.position.z
-        y = odom.pose.pose.position.x
-        euler = R.from_quat([odom.pose.pose.orientation.x,
-                             odom.pose.pose.orientation.y,
-                             odom.pose.pose.orientation.z,
-                             odom.pose.pose.orientation.w]).as_euler('xyz', degrees=True)
-        # get the yaw value from euler rotation angles
-        yaw = euler[1]
+        # get the pose
+        try:
+            trans = self.tfBuffer.lookup_transform('base_link', 'map', rospy.Time())
+        except Exception as e:
+            cprint('Exception :: ' + str(e), 'red')
+            return
+
+        x = trans.transform.translation.x
+        y = trans.transform.translation.y
+        quaternion = [trans.transform.rotation.x, trans.transform.rotation.y, trans.transform.rotation.z, trans.transform.rotation.w]
+
+        # convert quaternion to yaw angle
+        yaw = R.from_quat(quaternion).as_euler('xyz', degrees=True)[2]
 
         # get joystick data
         joy_axes = joystick.axes
         joy_buttons = joystick.buttons
 
-        # save the data
+        # append to the data
         self.data['pose'].append([x, y, yaw])
         self.data['bevlidarimg'].append(bev_lidar_image)
         self.data['joystick'].append([joy_axes, joy_buttons])
 
     def save_data(self, data_path):
         """ convert joystick values to cmd_vel """
-        cprint('Converting joystick values to cmd_vel.. ', 'yellow')
+        cprint('Converting joystick values to cmd_vel.. ', 'blue', attrs=['bold'])
         for i in tqdm(range(len(self.data['joystick']))):
             joy_axes = self.data['joystick'][i][0]
             joy_buttons = self.data['joystick'][i][1]
@@ -118,7 +133,7 @@ if __name__ == '__main__':
     # start a subprocess to run the rosbag
     rosbag_play_process = subprocess.Popen(['rosbag', 'play', rosbag_path, '-r', '1', '--clock'])
 
-    save_data_path = rosbag_path.replace('.bag', '_data.pkl')
+    save_data_path = os.path.join(rospy.get_param('save_data_path'), rosbag_path.split('/')[-1].replace('.bag', '_data.pkl'))
 
     datarecorder = ListenRecordData(rosbag_play_process = rosbag_play_process,
                                     config_path = config_file_path)
@@ -128,6 +143,7 @@ if __name__ == '__main__':
         if rosbag_play_process.poll() is not None:
             print('rosbag process has stopped')
             datarecorder.save_data(save_data_path)
+            print('Data was saved in :: ', save_data_path)
             exit(0)
 
     rospy.spin()
